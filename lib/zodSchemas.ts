5a5af950b596d393b18c5571a9af3e7d560ab712
Nik Farees <nikfarees2015@gmail.com>
2025-08-10 02:06:39 +0800

    Course Creation
    Introduces a new course creation page in the admin section with form validation using zod schemas. Adds a shared zod schema for course validation, updates the courses admin page with a 'Create Course' button, and integrates the 'slugify' package. Updates the Prisma schema to include a Course model and its relation to User, and bumps the zod dependency version.

---
+++ lib/zodSchemas.ts
@@ -0,0 +1,54 @@
+import { z } from 'zod';
+
+export const courseLevels = ["Beginner", "Intermediate", "Advanced"] as const;
+export const courseStatus = ["Draft", "Published", "Archived"] as const;
+
+export const courseCategories = [
+    "Development",
+    "Business",
+    "Finance",
+    "It & Softaware",
+    "Office Productivity",
+    "Personal Development",
+    "Design",
+    "Marketing",
+    "Health & Fitness",
+    "Music",
+    "Teaching & Academics",
+] as const;
+
+export const courseSchema = z.
+    object({
+        title: z
+            .string()
+            .min(3, { error: "Title must be at least 3 characters long" })
+            .max(100, { error: "Title must be at most 100 characters long" }),
+        description: z
+            .string()
+            .min(3, { error: "Description must be at least 3 characters long" }),
+        fileKey: z
+            .string()
+            .min(1, { error: "File key is required" }),
+        price: z
+            .number()
+            .min(1, { error: "Price must be at least 1" }),
+        duration: z
+            .number()
+            .min(1, { error: "Duration must be at least 1 hour" })
+            .max(500, { error: "Duration must be at most 500 hours" }),
+        level: z
+            .enum(courseLevels, { error: "Level is required" }),
+        category: z
+            .enum(courseCategories, { error: "Category is required" }),
+        smallDescription: z
+            .string()
+            .min(3, { error: "Small description must be at least 3 characters long" })
+            .max(200, { error: "Small description must be at most 200 characters long" }),
+        slug: z
+            .string()
+            .min(3, { error: "Slug must be at least 3 characters long" }),
+        status: z
+            .enum(courseStatus, { error: "Status is required" }),
+    });
+
+export type CourseSchemaType = z.infer<typeof courseSchema>;
