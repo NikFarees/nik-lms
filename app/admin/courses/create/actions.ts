6a7bed0d25eca0b23b8fadfea244f8f40071fe54
Nik Farees <nikfarees2015@gmail.com>
2025-08-13 01:54:31 +0800

    Protect Endpoints
    Introduces a requireAdmin utility to restrict certain actions and pages to admin users, redirecting non-admins to a new not-admin page. Adds Arcjet bot detection and rate limiting to course creation and S3 upload/delete API routes. Updates Prisma schema to support user roles and admin features, and enables admin plugin in authentication logic.

--- app/admin/courses/create/actions.ts
+++ app/admin/courses/create/actions.ts
@@ -1,20 +1,55 @@
 "use server";
 
-import { auth } from "@/lib/auth";
+import { requireAdmin } from "@/app/data/admin/require-admin";
+import arcjet, { detectBot, fixedWindow } from "@/lib/arcjet";
 import { prisma } from "@/lib/db";
 import { ApiResponse } from "@/lib/types";
 import { courseSchema, CourseSchemaType } from "@/lib/zodSchemas";
-import { headers } from "next/headers";
+import { request } from "@arcjet/next";
+
+const aj = arcjet
+    .withRule(
+        detectBot({
+            mode: "LIVE",
+            allow: [],
+        })
+    )
+    .withRule(
+        fixedWindow({
+            mode: "LIVE",
+            window: "1m",
+            max: 5,
+        })
+    );
 
 export async function CreateCourse(values: CourseSchemaType): Promise<ApiResponse> {
-    
+
+    const session = await requireAdmin();
+
     try {
-        const session = await auth.api.getSession({
-            headers: await headers(),
+        const req = await request();
+
+        const decision = await aj.protect(req, {
+            fingerprint: session?.user.id,
         });
+
+        if (decision.isDenied()) {
+            if (decision.reason.isRateLimit()) {
+                return {
+                    status: "error",
+                    message: "You have been blocked due to rate limiting.",
+                };
+            } else {
+                return {
+                    status: "error",
+                    message: "You are a bot! if this is a mistake, please contact support.",
+                };
+            }
+        }
+
         const validation = courseSchema.safeParse(values);
 
-        if(!validation.success) {
+        if (!validation.success) {
             return {
                 status: "error",
                 message: "Invalid Form Data",
--- app/api/s3/upload/route.ts
+++ app/api/s3/upload/route.ts
@@ -5,16 +5,44 @@ import { z } from 'zod';
 import { v4 as uuidv4 } from 'uuid';
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 import { S3 } from '@/lib/S3Client';
+import arcjet, { detectBot, fixedWindow } from '@/lib/arcjet';
+import { requireAdmin } from '@/app/data/admin/require-admin';
 
 export const fileUploadSchema = z.object({
     fileName: z.string().min(1, { error: "File name is required" }),
     contentType: z.string().min(1, { error: "Content type is required" }),
     size: z.number().min(1, { error: "Size is required" }),
     isImage: z.boolean(),
-})
+});
+
+const aj = arcjet
+    .withRule(
+        detectBot({
+            mode: "LIVE",
+            allow: [],
+        })
+    )
+    .withRule(
+        fixedWindow({
+            mode: "LIVE",
+            window: "1m",
+            max: 5,
+        })
+    );
 
 export async function POST(request: Request) {
+
+    const session = await requireAdmin();
+
     try {
+        const decision = await aj.protect(request, {
+            fingerprint: session?.user.id as string,
+        });
+
+        if (decision.isDenied()) {
+            return NextResponse.json({ error: "Request denied" }, { status: 429 });
+        }
+
         const body = await request.json();
 
         const validation = fileUploadSchema.safeParse(body);
@@ -26,7 +54,7 @@ export async function POST(request: Request) {
             );
         }
 
-        const{fileName, contentType, size} = validation.data;
+        const { fileName, contentType, size } = validation.data;
 
         const uniqueKey = `${uuidv4()}-${fileName}`
 
@@ -53,6 +81,6 @@ export async function POST(request: Request) {
             { error: "Failed to generate presigned URL" },
             { status: 500 }
         );
-     }
+    }
 }
 
