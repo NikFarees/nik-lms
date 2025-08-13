5799c2af87e1483e044a32b60840a08ae009faad
Nik Farees <nikfarees2015@gmail.com>
2025-08-13 22:50:08 +0800

    Add admin courses page with course card and data fetching
    Introduces an admin courses page that fetches and displays courses using a new data access function (`adminGetCourses`). Adds an `AdminCourseCard` component for rendering course details, a `useConstructUrl` hook for image URLs, and updates Next.js config to allow remote images from the S3 bucket.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        hostname: "nik-lms-yt-video-subscribe.t3.storageapi.dev",
+        port: "",
+        protocol: "https",
+      },
+    ],
+  }
 };
 
 export default nextConfig;
